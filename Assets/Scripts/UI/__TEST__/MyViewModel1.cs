48f9a038afaffa85a809f69abe25b02c041cc3c8
Arkadiy <91030077+Puhovon@users.noreply.github.com>
2024-09-25 01:55:12 +0300

    add Unity weld MVVM

---
+++ Assets/Scripts/UI/__TEST__/MyViewModel1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityWeld.Binding;
+
+namespace UI.__TEST__
+{
+    [Binding]
+    public class MyViewModel1 : MonoBehaviour, INotifyPropertyChanged
+    {
+        private float _timer = 0;
+        private string _text = "text";
+        
+        [Binding]
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (_text == value)
+                {
+                    return;
+                }
+
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            if (_timer > 10f)
+            {
+                Text = "New text";
+            }
+        }
+    }
+}
